325c981042b7f2e10362eec48f17b097d4d48afc
Mauro Gestoso <maurogestoso@gmail.com>
2018-06-29 08:25:41 +0100

    Add initial notes on binary-tree

--- coding-train/binary-tree/index.js
+++ coding-train/binary-tree/index.js
@@ -1,7 +1,29 @@
+let tree;
+
 function setup () {
-    
+    noCanvas();
+    tree = new Tree();
+
+    tree.addNode(5);
+    console.log(tree);
 }
 
-function draw () {
-    ellipse(width/2, height/2, 32, 32)
+class Node {
+    constructor (val) {
+        this.value = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor () {
+        this.root = null;
+    }
+    addNode (val) {
+        const n = new Node(val);
+        if (!this.root) {
+            this.root = n;
+        }
+    }
 }
