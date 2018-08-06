dca9baf4167346546c2c788d76beed99347b59b4
Mauro Gestoso <mauro.gestoso@bbc.co.uk>
2018-08-06 09:23:20 +0100

    [udemy-react-ssr] Webpack improvements

--- udemy-react-ssr/app-server/webpack.server.js
+++ udemy-react-ssr/app-server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base");
+const webpackNodeExternals = require("webpack-node-externals");
 
 const serverConfig = {
   // Inform Webpack that we're building a bundle for Node
@@ -13,7 +14,11 @@ const serverConfig = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build")
-  }
+  },
+
+  // excludes from the bundle external dependencies
+  // i.e. dependencies in node_modules
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, serverConfig);
